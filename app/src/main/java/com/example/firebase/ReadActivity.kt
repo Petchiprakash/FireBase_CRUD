d62d68786c7faeb083eb46b27dbaf1f176268a45
Petchiprakash <petchiprakash2@gmail.com>
2021-12-22 22:27:07 +0530

    Logic Added for lastName Search

--- app/src/main/java/com/example/firebase/ReadActivity.kt
+++ app/src/main/java/com/example/firebase/ReadActivity.kt
@@ -1,8 +1,8 @@
 package com.example.firebase
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.example.firebase.databinding.ActivityReadBinding
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -16,26 +16,35 @@ class ReadActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.btnRead.setOnClickListener {
-            val phoneNo: String = binding.etPhoneNo.text.toString()
-            if (phoneNo.isNotEmpty()) {
-                readData(phoneNo)
+            val lastN: String = binding.etLastName.text.toString()
+            if (lastN.isNotEmpty()) {
+                readData(lastN)
             } else {
-                Toast.makeText(this, "Please Enter The Phone Number", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Please Enter The Last Name", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     private fun readData(phoneNo: String) {
-        database= FirebaseDatabase.getInstance().getReference("Users")
-        database.child(phoneNo).get().addOnSuccessListener {
-            if (it.exists()){
-                binding.etPhoneNo.text.clear()
-                val firstName = it.child("firstName").value
-                val lastName =it.child("lastName").value
-                val phoneNumber =it.child("phoneNo").value
-                Toast.makeText(this, "$firstName \n $lastName \n $phoneNumber", Toast.LENGTH_LONG).show()
-            }else{
-                Toast.makeText(this, "User Doesn't Exist", Toast.LENGTH_SHORT).show()
+        var count = 0
+        database = FirebaseDatabase.getInstance().getReference("Users")
+        database.get().addOnSuccessListener {
+            for (participantList in it.children) {
+                if (phoneNo == participantList.child("lastName").value) {
+//                    println(participantList.key)
+                    binding.etLastName.text.clear()
+                    val firstName = participantList.child("firstName").value
+                    val lastName = participantList.child("lastName").value
+                    val phoneNumber = participantList.child("phoneNo").value
+                    Toast.makeText(
+                        this,
+                        "$firstName \n $lastName \n $phoneNumber",
+                        Toast.LENGTH_LONG
+                    ).show()
+                    count ++
+                } else if(count<0){
+                    Toast.makeText(this, "User Doesn't Exist", Toast.LENGTH_SHORT).show()
+                }
             }
         }.addOnFailureListener {
             Toast.makeText(this, "Failed To Fetch Data", Toast.LENGTH_SHORT).show()
